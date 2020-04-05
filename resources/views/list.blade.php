4058a1149fc926af0574627eb7cce08f86c35dac
中村圭輝 <mac@nakamuranomac.local>
2020-04-05 15:34:03 +0900

    list画面を修正

---
+++ resources/views/list.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>Account Password Manager</title>
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <header>
+    <div class="header-left">
+      <h1>Account Password Manager</h1>
+    </div>
+    <div class="header-right">
+      <h3>登録</h3>
+      <h3>リスト</h3>
+    </div>
+  </header>
+  <div class="body">
+  	<div class="acpw">
+  		<h1>
+  			<a href="https://slack.com/intl/ja-jp/signin" target="_blank" class="sitename" class="sitename">
+  				SLACK
+  			</a>
+  		</h1>
+  		<table>
+  			<tr>
+  				<th>アカウント</th>
+  				<td>tel07360102@gmail.com</td>
+  			</tr>
+  			<tr>
+  				<th>パスワード</th>
+  				<td>09tmw3Mpo0Q</td>
+  			</tr>
+  		</table>
+  	</div>
+	  <div class="acpw">
+	  	<h1>
+	  		<a href="https://id.heroku.com/login" target="_blank" class="sitename">
+	  			Heroku
+	  		</a>
+	  	</h1>
+	  	<table>
+	  		<tr>
+	  			<th>アカウント</th>
+	  			<td>tel07360102@gmail.com</td>
+	  		</tr>
+	  		<tr>
+	  			<th>パスワード</th>
+	  			<td>ti4TeMp-89ju</td>
+	  		</tr>
+	  	</table>
+	  </div>
+	</div>
+</body>
+</html>
